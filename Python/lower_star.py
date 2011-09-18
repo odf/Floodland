0c6e4793ce19df7c3ad59385f2549b362a06b04e
odf <olaf.delgado@gmail.com>
2011-09-18 16:44:13 +1000

    More coding.

--- Python/lower_star.py
+++ Python/lower_star.py
@@ -19,10 +19,20 @@ def multi_enumerate(data):
     return reduce(f, range(asarray(data).ndim), (((), data),))
 
 
-def ranks(data):
+def ranking(data):
     t = sorted(tuple((v, i) for i, v in multi_enumerate(data)))
     return dict((k, i) for i, (v, k) in enumerate(t))
 
 
 def reverse_dict(d):
     return dict((v, k) for k, v in d.items())
+
+
+def star_index_lists(n):
+    if n == 0:
+        return (((), ((),)),)
+    else:
+        f = lambda s, t: tuple(a + (b,) for a in s for b in t)
+        return tuple((k + (m,), f(s, t))
+                     for k, s in star_index_lists(n-1)
+                     for m, t in enumerate(((0, 1), (1,), (1, 2))))
